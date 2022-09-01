502d1e39317cc5f36c38ea88f1b81eeeca0881d9
Attila Baczay <johnscape96@gmail.com>
2022-09-01 11:25:45 +0200

    Added comments to the header

--- include/NeuralNetwork/Layers/ConvLayer.h
+++ include/NeuralNetwork/Layers/ConvLayer.h
@@ -4,23 +4,94 @@
 
 class ActivationFunction;
 
+/**
+ * @brief A convolutional layer
+ */
 class ConvLayer : public Layer
 {
 public:
-	ConvLayer(Layer* inputLayer, unsigned int kernelSize, unsigned int stride, unsigned int nrFilters = 1,
+	/**
+	 * @brief Creates a new convolutional layer
+	 * @param inputLayer The layer to get the input from
+	 * @param kernelSize The size of the kernel: nxn
+	 * @param stride The stride, i.e.: the size of the steps the kernel makes
+	 * @param nrFilters The number of kernels used, determines the size of the output
+	 * @param minimumPad The minimum padding applied to the input
+	 * @param padType The type of padding applied
+	 * @param padFill If constant padding is used, this will be the value used to fill the pads
+	 */
+	ConvLayer(Layer* inputLayer, unsigned int kernelSize, unsigned int stride=1, unsigned int nrFilters = 1,
 			  unsigned int minimumPad = 0, Matrix::PadType padType = Matrix::PadType::CONSTANT, float padFill = 0);
 	virtual ~ConvLayer();
 
+	/**
+	 * @brief Creates a clone of this layer
+	 * @return The clone of this layer
+	 */
 	virtual Layer* Clone();
+
+	/**
+	 * @brief Computes the output of the layer
+	 */
 	virtual void Compute();
+
+	/**
+	 * @brief Computes the output and returns it
+	 * @return The updated output of the layer
+	 */
 	virtual Tensor& ComputeAndGetOutput();
+
+	/**
+	 * @brief Updates the layer's error for training purposes
+	 * @param error The error of the next layer
+	 * @param recursive Set to true if you want to train all previous layers
+	 */
 	virtual void GetBackwardPass(const Tensor& error, bool recursive = false);
+
+	/**
+	 * @brief Uses an optimizer to update the kernel based on the error
+	 * @param optimizer The optimizer to use
+	 */
 	virtual void Train(Optimizer* optimizer);
+
+	/**
+	 * @brief Returns the kernel of the layer
+	 * @return The kernel of the layer
+	 */
+	Tensor& GetKernel();
+
+	/**
+	 * @brief Returns the pad size used in the convolution
+	 * @return The size of the pad
+	 */
+	unsigned int GetPadSize();
+
+	/**
+	 * @brief Loads the layer from a JSON string.
+	 * @param data The JSON data or file name to load from.
+	 * @param isFile If the data is from a file, set it to true.
+	*/
 	virtual void LoadFromJSON(const char* data, bool isFile = false);
+
+	/**
+	 * @brief Loads the layer from JSON
+	 * @param jsonData rapidjsson value type, containing the data for the layer
+	 */
+	virtual void LoadFromJSON(rapidjson::Value& jsonData);
+
+	/**
+	 * @brief Saves the layer into a JSON string.
+	 * @param fileName If you want to save the JSON data into a file, enter the file name here.
+	 * @return A string containing the JSON describtion of the layer.
+	*/
 	virtual std::string SaveToJSON(const char* fileName = nullptr) const;
 
-	Tensor& GetKernel();
-	unsigned int GetPadSize();
+	/**
+	 * @brief Saves the layer into a JSON value object
+	 * @param document A reference for the top document object
+	 * @return A rapidjson value type containing the layer
+	 */
+	virtual rapidjson::Value SaveToJSONObject(rapidjson::Document& document) const;
 
 
 private:
