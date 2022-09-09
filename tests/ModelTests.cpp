91231214e5989d881a71eafee8aa51fc3ea49525
Attila Baczay <johnscape96@gmail.com>
2022-09-09 19:18:02 +0200

    Added tests to model

---
+++ tests/ModelTests.cpp
@@ -0,0 +1,159 @@
+#include <catch2/catch.hpp>
+#include "NeuralNetwork/Model.h"
+#include "NeuralNetwork/Layers/InputLayer.h"
+#include "NeuralNetwork/Layers/FeedForwardLayer.h"
+#include "NeuralNetwork/Optimizers/GradientDescent.h"
+#include "NeuralNetwork/LossFunctions/MSE.hpp"
+
+/**
+ * Needed tests:
+ * - Run model
+ * - Train model
+ * - Save model
+ * - Load model
+*/
+
+SCENARIO("Creating a model", "[model][init]")
+{
+	GIVEN("an empty model and a test input tensor")
+	{
+		Model model;
+		Tensor tensor({3, 5}, nullptr);
+		tensor.FillWith(1);
+
+		WHEN("checking properties")
+		{
+			THEN("the model has 0 layers")
+			{
+				REQUIRE(model.GetLayerCount() == 0);
+			}
+			THEN("the model input and output is nullpr")
+			{
+				REQUIRE(model.GetInput() == nullptr);
+				REQUIRE(model.GetOutput() == nullptr);
+			}
+			THEN("the model cannot be run")
+			{
+				REQUIRE_THROWS(model.Compute(tensor));
+			}
+			THEN("the model cannot be trained")
+			{
+				REQUIRE_THROWS(model.Train(nullptr));
+			}
+		}
+		WHEN("adding a few layers to the model")
+		{
+			InputLayer* inputLayer = new InputLayer(5);
+			FeedForwardLayer* outputLayer = new FeedForwardLayer(inputLayer, 8);
+
+			model.AddLayer(inputLayer);
+			model.AddLayer(outputLayer);
+
+			THEN("the model has 2 layers")
+			{
+				REQUIRE(model.GetLayerCount() == 2);
+			}
+			THEN("the model's input is the input layer")
+			{
+				REQUIRE(model.GetInput() == inputLayer);
+			}
+			THEN("the model's output is the output layer")
+			{
+				REQUIRE(model.GetOutput() == outputLayer);
+			}
+			THEN("the model's first layer is the input, the second is the output")
+			{
+				REQUIRE(model.GetLayerAt(0) == inputLayer);
+				REQUIRE(model.GetLayerAt(1) == outputLayer);
+			}
+		}
+	}
+}
+
+SCENARIO("Running the model", "[model][computation]")
+{
+	GIVEN("a two layer model")
+	{
+		InputLayer* inputLayer = new InputLayer(5);
+		FeedForwardLayer* outputLayer = new FeedForwardLayer(inputLayer, 3);
+
+		float weightValues[15] = {
+				0.5, 0, 0.5,
+				0.5, 0.5, 0,
+				0.5, 0, 0.5,
+				0.5, 0.5, 0,
+				0.5, 0, 0.5
+		};
+
+		Matrix tmp(5, 3, weightValues);
+
+		outputLayer->GetWeights().ReloadFromOther(tmp);
+		outputLayer->GetBias().FillWith(0);
+
+		Model model;
+		model.AddLayer(inputLayer);
+		model.AddLayer(outputLayer);
+
+		WHEN("getting an output")
+		{
+			float values[5] = {0, 1, 0, 1, 0};
+			Tensor input({1, 5}, values);
+
+			Tensor output = model.Compute(input);
+			THEN("the result should be the following")
+			{
+				float s1 = 0.7310586;
+				float outputValue[3] = {s1, s1, 0};
+				Tensor expected({1, 3}, outputValue);
+				Tensor diff = output - expected;
+
+				REQUIRE(abs(diff.Sum()) < 0.01);
+			}
+		}
+	}
+}
+
+SCENARIO("Training the model", "[model][training]")
+{
+	GIVEN("a two layer model")
+	{
+		InputLayer* inputLayer = new InputLayer(5);
+		FeedForwardLayer* outputLayer = new FeedForwardLayer(inputLayer, 3);
+
+		float weightValues[15] = {
+				0.5, 0, 0.5,
+				0.5, 0.5, 0,
+				0.5, 0, 0.5,
+				0.5, 0.5, 0,
+				0.5, 0, 0.5
+		};
+
+		Matrix tmp(5, 3, weightValues);
+
+		outputLayer->GetWeights().ReloadFromOther(tmp);
+		outputLayer->GetBias().FillWith(0);
+
+		Model model;
+		model.AddLayer(inputLayer);
+		model.AddLayer(outputLayer);
+
+		WHEN("training the model")
+		{
+			float values[5] = {0, 1, 0, 1, 0};
+			Tensor input({1, 5}, values);
+
+			float expected[3] = {1, 0, 1};
+			Tensor expectedOutput({1, 3}, expected);
+
+			Tensor output1 = model.Compute(input);
+			MSE errorFunc;
+			float error1 = errorFunc.Loss(output1, expectedOutput);
+
+			GradientDescent gradientDescent(&errorFunc, &model, 0.5);
+			gradientDescent.Train(input, expectedOutput);
+			Tensor output2 = model.Compute(input);
+			float error2 = errorFunc.Loss(output2, expectedOutput);
+			REQUIRE(error2 < error1);
+		}
+	}
+}
